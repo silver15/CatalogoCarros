9d75e5cc7e316117d9458560f8c02d0b4fb6da56
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-08-15 22:02:41 -0300

    Navbar Styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,13 @@
-
+import Navbar from './components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello Catalogo de Carros</h1>
+    <>
+    <Navbar />
+     <h1>Hello Catalogo de Carros</h1>
+     </>
   );
 }
 
